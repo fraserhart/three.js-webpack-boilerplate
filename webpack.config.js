18ffbb303924c6c968737167ceb17f7a1d3f4da3
Fraser Hart <fraser.hart@mkodo.com>
2017-11-17 14:59:16 +0000

    html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,32 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    entry: './src/app.js',
-    output: {
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            test: /\.scss$/,
-            use: [{
-                loader: "style-loader" // creates style nodes from JS strings
-            }, {
-                loader: "css-loader" // translates CSS into CommonJS
-            }, {
-                loader: "sass-loader" // compiles Sass to CSS
-            }]
-        },
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: "eslint-loader",
-            options: {
-                configFile: 'eslint-config-airbnb'
-            }
-        }]
-    }
+  entry: './src/app.js',
+  output: {
+    filename: './build/bundle.js',
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [{
+        loader: 'style-loader', // creates style nodes from JS strings
+      }, {
+        loader: 'css-loader', // translates CSS into CommonJS
+      }, {
+        loader: 'sass-loader', // compiles Sass to CSS
+      }],
+    }, {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'eslint-loader',
+      options: {
+        configFile: 'eslint-config-airbnb',
+      },
+    }],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+    }),
+  ],
 };
